e18d6762be0ba00ec8b3b4ce6fb3e24b6d214128
adfriend45 <adf10@cam.ac.uk>
2016-12-21 14:44:20 +0000

    Just changed date.

--- SOURCE/HYBRID9.f90
+++ SOURCE/HYBRID9.f90
@@ -6,7 +6,7 @@ PROGRAM H9
 ! Global simulation of land surface water and carbon fluxes.
 ! Uses PGF forcings, 1901-2012.
 ! Andrew D. Friend
-! 21st December, 2016
+! 30th November, 2016
 !----------------------------------------------------------------------!
 
 !----------------------------------------------------------------------!
